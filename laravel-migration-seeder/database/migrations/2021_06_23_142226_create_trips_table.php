19a4387357d1ac09f72138cdc964089a4d7723a3
NoeFabris <noe@elysresidence.com>
2021-06-23 16:57:41 +0200

    First Migrate

---
+++ laravel-migration-seeder/database/migrations/2021_06_23_142226_create_trips_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->char('destination_country_ISO', 3);
+            $table->mediumInteger('total_price');
+            $table->text('trip_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
